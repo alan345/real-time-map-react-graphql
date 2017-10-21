73651c991659cc0827f6aa63739079816658e65a
alan345 <alan345@gmail.com>
2017-10-21 11:25:06 -0700

    router integrated

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from './Map';
 import './index.css';
+import { Router, Route, browserHistory } from 'react-router'
+import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo'
+
+
+const networkInterface = createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj90jghsc0ld701749s7lmky7' })
+
+
+const client = new ApolloClient({ networkInterface })
 
 ReactDOM.render(
-  <Map />,
+  <ApolloProvider client={client}>
+  <Router history={browserHistory}>
+    <Route path='/' component={Map} />
+    <Route path='create' component={Map} />
+  </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 )
